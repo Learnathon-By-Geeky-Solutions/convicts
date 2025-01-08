797b77e8607afaccd6edf94579edfd8aa692afcc
orpheus <66983935+orpheus6678@users.noreply.github.com>
2025-01-09 01:32:02 +0600

    .gitignore now an ignore file for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path"
+import path from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
 import eslintConfigPrettier from "eslint-config-prettier"
+import { includeIgnoreFile } from "@eslint/compat"
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, ".gitignore")
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
 const eslintConfig = [
+  includeIgnoreFile(gitignorePath),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
 ]
