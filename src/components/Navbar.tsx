56f0bdd49be40fef1a6b072f775aede173bf546f
orpheus <66983935+orpheus6678@users.noreply.github.com>
2025-02-05 02:42:24 +0600

    Fix signIn issue
    Navbar.tsx moved from components/ui to components
    in Navbar.tsx signIn is now imported from @/auth instead of next-auth/react
    Removed accidentally caused redundancies in tailwind.config.ts
    Set prettier to use spaces (applied immediately to page, layout.tsx and tailwind.config.ts)
    Updated Jest snapshots so tests pass

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import Form from "next/form"
-import { signIn } from "next-auth/react"
+import { signIn } from "@/auth"
 
-const Navbar = () => {
+async function Navbar() {
   return (
-    <div className="bg-black px-5 py-3 font-sans shadow-sm">
+    <div className="border-4 px-5 py-3 font-sans shadow-sm">
       <nav className="flex items-center justify-between">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={144} />
